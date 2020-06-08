662c0bddfb2f8f6f02150892930c5cd3dfe50689
Administrator <jerrytiencn@126.com>
2020-06-08 17:38:13 +0800

    提交到本地仓库

---
+++ git-learning/src/main/java/com/tianzy/enitities/User.java
@@ -0,0 +1,12 @@
+package com.tianzy.enitities;
+
+/**
+ * @Author: Tianzy
+ * @Descripition:
+ * @Date:Created in 17:25 2020/6/8
+ * @Modidfied By:
+ */
+public class User {
+    private String name;
+    private Integer age;
+}
